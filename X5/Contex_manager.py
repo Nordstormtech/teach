903b60fc4614685a94dce3a84c71ce97b8bf72b4
Scaandinav <skandinavian31@yandex.ru>
2023-03-22 13:57:35 +0300

    context manager 2 type

--- X5/Contex_manager.py
+++ X5/Contex_manager.py
@@ -1,6 +1,8 @@
 import sqlite3
+from contextlib import contextmanager
 
 
+# Type 1
 class DataConn:
 
     def __init__(self, db_name):
@@ -23,8 +25,39 @@ class DataConn:
             raise
 
 
-if __name__ == '__main__':
-    db = 'test.db'
+# Type 1
+# if __name__ == '__main__':
+#    db = 'test.db'
+#
+#    with DataConn(db) as conn:
+#       cursor = conn.cursor()
 
-    with DataConn(db) as conn:
-        cursor = conn.cursor()
+# Type 2
+
+class Resource:
+    def __init__(self, db_name):
+        """Конструктор"""
+        self.db_name = db_name
+
+    def open(self):
+        print("open")
+
+    def close(self):
+        print('close')
+
+
+@contextmanager
+def open_resource():
+    resource = None
+    try:
+        resource = Resource('test')
+        resource.open()
+        yield resource
+    except:
+        raise
+    finally:
+        if resource:
+            resource.close()
+
+
+open_resource()
